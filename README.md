0bb7eb317ff34aceac16c6f8b77d782ded617442
DonOutcast <sham1996@yandex.ru>
2022-07-11 12:41:16 +0300

    your need add docstring

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# jetbrains-academy-simple-chatty-bot
+# Multiple choice
+
+## Description
+At the final stage, you will improve your simple bot so that it can give you a test and check your answers. The test should be a multiple-choice quiz about programming. Your bot has to repeat the test until you answer correctly and congratulate you upon completion.
+
+## Objective
+Your bot can ask anything you want, but there are two rules for your output:
+
+- the line with the test should end with the question mark character;
+- an option starts with a digit followed by the dot (1., 2., 3., 4.)
+If a user enters an incorrect answer, the bot may print a message:
+
+Please, try again.
+The program should stop on the correct answer and print Congratulations, have a nice day! at the end.
+
+## Example
+The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
+
+Example 1: a dialogue with the final version of your bot <br>
+![scrin](misc/images/cron.png)
+
+The program must end with the Congratulations, have a nice day! message.
+
+Use the provided template to simplify your work. You can change the text if you want. Please note that we use functions to make it easy to understand the program and add new code to it or edit later.
+
--- src/chatty_bot.py
+++ src/chatty_bot.py
@@ -1,58 +1,67 @@
-def greet(bot_name: str, birth_year: str) -> None:
-    print('Hello! My name is ' + bot_name + '.')
-    print('I was created in ' + birth_year + '.')
+class Chatty:
+
+    def __init__(self, bot_name: str, birth_year: str) -> None:
+        self.bot_name = bot_name
+        self.birth_year = birth_year
+
+    def greet(self) -> None:
+        print('Hello! My name is ' + self.bot_name + '.')
+        print('I was created in ' + self.birth_year + '.')
+
+    @staticmethod
+    def remind_name() -> None:
+        print('Please, remind me your name.')
+        name = input()
+        print('What a great name you have, ' + name + '!')
+
+    @staticmethod
+    def guess_age() -> None:
+        print('Let me guess your age.')
+        print('Enter remainders of dividing your age by 3, 5 and 7.')
+        rem3 = int(input())
+        rem5 = int(input())
+        rem7 = int(input())
+        age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+        print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+    @staticmethod
+    def count() -> None:
+        print('Now I will prove to you that I can count to any number you want.')
+        num = int(input())
+        curr = 0
+        while curr <= num:
+            print(curr, '!')
+            curr = curr + 1
+
+    @staticmethod
+    def test() -> None:
+        print("Let's test your programming knowledge.")
+        answer = 0
+        while answer != 2:
+            print("Why do we use methods?")
+            print("1. To repeat a statement multiple times.")
+            print("2. To decompose a program into several small subroutines.")
+            print("3. To determine the execution time of a program.")
+            print("4. To interrupt the execution of a program.")
+            if answer != 2:
+                print("Please, try again.")
+                answer = int(input())
+        else:
+            print('Completed, have a nice day!')
+
+    @staticmethod
+    def end() -> None:
+        print('Congratulations, have a nice day!')
 
 
-def remind_name() -> None:
-    print('Please, remind me your name.')
-    name = input()
-    print('What a great name you have, ' + name + '!')
+bot = Chatty("Outcast", "2020")
+bot.greet()
+bot.remind_name()
+bot.guess_age()
+bot.count()
+bot.test()
+bot.end()
 
 
-def guess_age() -> None:
-    print('Let me guess your age.')
-    print('Enter remainders of dividing your age by 3, 5 and 7.')
-
-    rem3 = int(input())
-    rem5 = int(input())
-    rem7 = int(input())
-    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-
-    print("Your age is " + str(age) + "; that's a good time to start programming!")
-
-
-def count() -> None:
-    print('Now I will prove to you that I can count to any number you want.')
-
-    num = int(input())
-    curr = 0
-    while curr <= num:
-        print(curr, '!')
-        curr = curr + 1
-
-
-def test() -> None:
-    print("Let's test your programming knowledge.")
-    answer = 0
-    while answer != 2:
-        print("Why do we use methods?")
-        print("1. To repeat a statement multiple times.")
-        print("2. To decompose a program into several small subroutines.")
-        print("3. To determine the execution time of a program.")
-        print("4. To interrupt the execution of a program.")
-        answer = int(input())
-        print("Please, try again.")
-    print('Completed, have a nice day!')
-
-
-def end() -> None:
-    print('Congratulations, have a nice day!')
-
-
-greet('Don Outcast', '2022')  # change it as you need
-remind_name()
-guess_age()
-count()
-test()
-end()
-
+if __name__ == "__main__":
+    pass
